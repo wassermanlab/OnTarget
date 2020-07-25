ed9ad7856193e796ea2cd511a1caafa2f3196549
tamario <avshalom.tamar0@gmail.com>
2020-07-24 17:10:30 -0700

    prototype for design module

--- app/src/components/design.js
+++ app/src/components/design.js
@@ -1,30 +1,36 @@
-import React, { useState, forwardRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavButtons from './navButtons';
 import IGV from './igv'
 import { ReactSortable } from "react-sortablejs";
 
 
-const CustomComponent = forwardRef((props, ref) => {
-  return (
+function Construct(props) {
+  // regulatoryRegions, genomeSize
+  const [c, setC] = useState([])
 
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Type</th>
-          <th scope="col">Core Region Score</th>
-          <th scope="col">Start</th>
-          <th scope="col">End</th>
-          <th scope="col">bp before start</th>
-          <th scope="col">bp after end</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.children}
-      </tbody>
-    </table>
-  )
-});
+
+  useEffect(() => {
+    let offset = 0;
+    let newC = []
+    props.regulatoryRegions.forEach(e=>{
+      if(e.inConstruct) {
+        const lenPercentage = (((e.end + e.after_end) - (e.start - e.before_start))/props.genomeSize)*100;
+        const y2 = offset + lenPercentage;
+        newC.push({"type": e.type, "name": e.name, "y1": offset, "y2": y2})
+        offset = y2;
+      }
+    })
+    setC(newC);
+  }, [props.regulatoryRegions, props.genomeSize]);
+  let offset = 0;
+  return (
+    <g>
+    {c.map((item, index) => {
+      return (<line x1={`${item.y1}%`} y1="20" x2={`${item.y2}%`} 
+      y2="20" style={{ stroke: (item.type==="promoter"? "orange": "blue"), strokeWidth: 10 }} />)
+    })}
+    </g>)
+}
 
 
 export default function Design(props) {
@@ -44,14 +50,11 @@ export default function Design(props) {
     console.log("do something and download")
   }
 
-  function checkWarnings(regs) {
-    // more than 1 promoter, construct too long, regions overlap eachother
-    // todo add region overlap eachother check
+  useEffect(() => {
     let newWarnings = [];
     let total_size = 0;
     let num_promoters = 0;
-    regs.forEach(element => {
-      console.log(element)
+    regulatoryRegions.forEach(element => {
       if (element.inConstruct) {
         if (element.type === "promoter") {
           num_promoters++;
@@ -69,22 +72,21 @@ export default function Design(props) {
       newWarnings.push("Construct is: " + total_size + "bp larger then available size(" + designSize + "bp).")
     }
     setWarnings(newWarnings);
-  }
+  }, [regulatoryRegions]);
 
   function changeConstruct(e) {
     const index = e.target.getAttribute('index');
     let newRegulatoryRegions = Array.from(regulatoryRegions);
     newRegulatoryRegions[index]["inConstruct"] = !newRegulatoryRegions[index]["inConstruct"];
-    checkWarnings(newRegulatoryRegions);
     setRegulatoryRegions(newRegulatoryRegions);
   }
 
   function handleChange(e) {
-    const type = e.target.getAttribute('key');
+    const type = e.target.getAttribute('objectkey');
     const index = e.target.getAttribute('index');
     const value = e.target.value;
     let newRegulatoryRegions = Array.from(regulatoryRegions);
-    newRegulatoryRegions[index][type] = value;
+    newRegulatoryRegions[index][type] = parseInt(value);
     setRegulatoryRegions(newRegulatoryRegions)
   }
 
@@ -93,37 +95,33 @@ export default function Design(props) {
       <h1 className="text-center">Design</h1>
       <hr />
 
-      <svg width="100%" height="50">
-        <g> {/* available space */}
-          <rect x="0" y="0" width={`${(designSize / genomeSize) * 100}%`} height="100%"
-            style={{ fill: "white", stroke: "black", strokeWidth: 1, }} />
-        </g>
-        <g> {/* cargo */}
-          <rect x={`${(designSize / genomeSize) * 100}%`} y="0"
-            width={`${(cargoSize / genomeSize) * 100}%`} height="100%"
-            style={{ fill: "black", stroke: "black", strokeWidth: 1, }} />
-          <line x1={`${(designSize / genomeSize) * 100}%`}
-            y1="0" x2={`${(designSize / genomeSize) * 100}%`} y2="100%"
-            style={{ stroke: "green", strokeWidth: 3 }} />
-        </g>
+      <svg width="100%" height="100">
+
+        {/* available space */}
+        <Construct regulatoryRegions={regulatoryRegions} genomeSize={genomeSize} />
+        <line x1="0" y1="90" x2={`${(designSize / genomeSize) * 100}%`} y2="90"
+          style={{ stroke: "green", strokeWidth: 10 }} />
+        {/* available space */}
+        <line x1={`${(designSize / genomeSize) * 100}%`} y1="90" x2="100%" y2="90"
+          style={{ stroke: "red", strokeWidth: 10 }} />
       </svg>
 
       <hr />
 
       <h2>Available Regions</h2>
       <div>
-        <div class="input-group mb-3">
-          <div class="input-group-prepend">
-            <span class="input-group-text">name</span>
-            <span class="input-group-text">type</span>
-            <span class="input-group-text">score</span>
-            <span class="input-group-text">start</span>
-            <span class="input-group-text">end</span>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text">name</span>
+            <span className="input-group-text">type</span>
+            <span className="input-group-text">score</span>
+            <span className="input-group-text">start</span>
+            <span className="input-group-text">end</span>
           </div>
-          <div class="input-group-append">
-            <span class="input-group-text">bp before start</span>
-            <span class="input-group-text">bp after end</span>
-            <span class="input-group-text">include in construct</span>
+          <div className="input-group-append">
+            <span className="input-group-text">bp before start</span>
+            <span className="input-group-text">bp after end</span>
+            <span className="input-group-text">include in construct</span>
           </div>
 
         </div>
@@ -133,24 +131,24 @@ export default function Design(props) {
 
         {regulatoryRegions.map((item, index) => {
           return (
-            <div>
-              <div class="input-group mb-3">
+            <div key={index}>
+              <div className="input-group mb-3">
                 {/* stuff before */}
-                <div class="input-group-prepend">
-                  <span class="input-group-text">{item.name}</span>
-                  <span class="input-group-text">{item.type}</span>
-                  <span class="input-group-text">{item.score}</span>
-                  <span class="input-group-text">{item.start}</span>
-                  <span class="input-group-text">{item.end}</span>
+                <div className="input-group-prepend">
+                  <span className="input-group-text">{item.name}</span>
+                  <span className="input-group-text">{item.type}</span>
+                  <span className="input-group-text">{item.score}</span>
+                  <span className="input-group-text">{item.start}</span>
+                  <span className="input-group-text">{item.end}</span>
                 </div>
                 {/* text inputs */}
-                <input type="number" class="form-control" placeholder="bp before start"
-                  onChange={handleChange} key="before_start" index={index} />
-                <input type="number" class="form-control" placeholder="bp after end"
-                  onChange={handleChange} key="after_end" index={index} />
+                <input type="number" className="form-control" placeholder="bp before start"
+                  onChange={handleChange} objectkey="before_start" index={index} />
+                <input type="number" className="form-control" placeholder="bp after end"
+                  onChange={handleChange} objectkey="after_end" index={index} />
                 {/* checkmark at end */}
-                <div class="input-group-append">
-                  <div class="input-group-text">
+                <div className="input-group-append">
+                  <div className="input-group-text">
                     <input type="checkbox" onChange={changeConstruct} index={index}
                       aria-label="Checkbox for following text input" />
                   </div>
@@ -161,8 +159,8 @@ export default function Design(props) {
         })}
       </ReactSortable>
 
-      {warnings.map(item => {
-        return (<div class="alert alert-warning" role="alert">
+      {warnings.map((item, index) => {
+        return (<div key={index} className="alert alert-warning" role="alert">
           {item}
         </div>)
       })}
