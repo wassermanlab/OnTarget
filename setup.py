df6dfb9f2627bd1664b13867c6006cb058c18d0b
Oriol Fornes <oriol.fornes@gmail.com>
2022-12-18 13:48:52 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+OnTarget: bioinformatics design of MiniPromoters for targeted delivery of expression
+"""
+
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="ontarget",
+    version="22.12.1",
+    author="Oriol Fornes, Tamar Av-Shalom",
+    author_email="oriol.fornes@gmail.com, avshalom.tamar0@gmail.com",
+    description="OnTarget: bioinformatics design of MiniPromoters for targeted delivery of expression",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wassermanlab/OnTarget",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
