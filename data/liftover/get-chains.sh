69ae97868704f084da7db3af61a63d20a2fef0d2
oriolfornes <oriol.fornes@gmail.com>
2022-12-19 09:27:36 -0800

    updated getting chains

--- data/liftover/get-chains.sh
+++ data/liftover/get-chains.sh
@@ -5,27 +5,22 @@
 # mm10Tohg38) from UCSC
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-if [ ! -f liftOver ]; then
-    wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/liftOver
-    chmod u+x liftOver
-fi
-
 CHAIN="hg19ToMm10.over.chain.gz"
 if [ ! -f ${CHAIN} ]; then
-    wget https://hgdownload.cse.ucsc.edu/goldenpath/hg19/liftOver/${CHAIN}
+    curl -O https://hgdownload.cse.ucsc.edu/goldenpath/hg19/liftOver/${CHAIN}
 fi
 
 CHAIN="mm9ToMm10.over.chain.gz"
 if [ ! -f ${CHAIN} ]; then
-    wget https://hgdownload.cse.ucsc.edu/goldenpath/mm9/liftOver/${CHAIN}
+    curl -O https://hgdownload.cse.ucsc.edu/goldenpath/mm9/liftOver/${CHAIN}
 fi
 
 CHAIN="mm10ToHg19.over.chain.gz"
 if [ ! -f ${CHAIN} ]; then
-    wget https://hgdownload.cse.ucsc.edu/goldenpath/mm10/liftOver/${CHAIN}
+    curl -O https://hgdownload.cse.ucsc.edu/goldenpath/mm10/liftOver/${CHAIN}
 fi
 
 CHAIN="mm10ToHg38.over.chain.gz"
 if [ ! -f ${CHAIN} ]; then
-    wget https://hgdownload.cse.ucsc.edu/goldenpath/mm10/liftOver/${CHAIN}
+    curl -O https://hgdownload.cse.ucsc.edu/goldenpath/mm10/liftOver/${CHAIN}
 fi
